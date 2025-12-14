613df30a57bed289b74819d3cc4a590c80953097
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2025-12-14 18:06:59 +0530

    Finishing the Drawer Navigation App

--- expo-router-drawer-navigation/app/_layout.tsx
+++ expo-router-drawer-navigation/app/_layout.tsx
@@ -1,5 +1,54 @@
-import { Stack } from "expo-router";
+import { Drawer } from "expo-router/drawer";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#ffffff",
+          },
+          headerTintColor: "#8b5cf6",
+          drawerStyle: {
+            backgroundColor: "#ffffff",
+          },
+          drawerActiveTintColor: "#8b5cf6",
+          drawerInactiveTintColor: "#64748b",
+        }}
+      >
+        <Drawer.Screen
+          name="index"
+          options={{
+            title: "Home title",
+            drawerLabel: "Home label",
+            drawerIcon: ({ color }) => (
+              <FontAwesome name="home" color={color} size={24} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="dashboard"
+          options={{
+            title: "Dashboard title",
+            drawerLabel: "Dashboard label",
+            drawerIcon: ({ color }) => (
+              <FontAwesome name="dashboard" color={color} size={24} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="settings"
+          options={{
+            title: "Settings title",
+            drawerLabel: "Settings label",
+            drawerIcon: ({ color }) => (
+              <FontAwesome name="cog" color={color} size={24} />
+            ),
+          }}
+        />
+      </Drawer>
+    </GestureHandlerRootView>
+  );
 }
